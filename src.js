5408b3ae597c18990a131aaf3f586a12755e2ab2
Asharfdev <87210166+Asharfdev@users.noreply.github.com>
2026-09-24 22:18:01 +0100

    Add main menu and candidate options to src.js

--- src.js
+++ src.js
@@ -1,1 +1,23 @@
 
+
+
+////////////Menu principale///////////
+console.log("1. Ajouter un condidat");
+console.log("2. afficer las list de candidates");
+console.log("3. voter ");
+console.log("4. modifier les information ");
+console.log("5. suprimer un candidat ");
+console.log("6. rechercher ");
+console.log("7. statistique ");
+
+
+///////////////Menu apres ajpouter un candidat//////
+
+console.log("1.Ajouter Un Candidat");
+console.log("2.Ajouter plusier candidat");
+console.log("0. revenire au menu parincipale");
+
+
+
+
+
