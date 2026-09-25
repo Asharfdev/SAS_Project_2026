a1cab6efb7bccff92a4a24d0726baa7f276c4d0d
Asharfdev <87210166+Asharfdev@users.noreply.github.com>
2026-09-25 11:47:59 +0100

    Update src.js

--- src.js
+++ src.js
@@ -1,39 +1,110 @@
 const read = require("prompt-sync")();
 
 const candidats = [
+  {
+	cin : "AB123456",
+	nom : "Boushaba",
+	prenom : "Soufiane",
+	partiPolitique : "Indépendant",
+	age: 40,
+	electeurs: []
+},
 
 {
-cin : "AB1231",
-nom : "Boushaba",
-prenom : "Soufiane",
-partiPolitique : "Indépendant",
-age: 40,
-electeurs: []
-},
-{
-cin : "AB1232",
-nom : "Boushaba",
-prenom : "Soufiane",
-partiPolitique : "Indépendant",
-age: 40,
-electeurs: []
-},
-{
-cin : "AB1233",
-nom : "Boushaba",
-prenom : "Soufiane",
-partiPolitique : "Indépendant",
-age: 40,
-electeurs: []
-},
+	cin : "AB123456",
+	nom : "Boushaba",
+	prenom : "Soufiane",
+	partiPolitique : "Indépendant",
+	age: 40,
+	electeurs: []
+}
+
 ];
 
+choix = 0 ;
 
-let choix = 0;
+do {
+      
+        //////////////Called Functions///// 
+        affichageCandidats()
+        //////////////////////////////////
 
-while (choix !== 8 ) {
-        ////////////Menu principale///////////
-        console.log("*********  e1lections ******");
+
+        choix  = +read("enter votre choix :")
+
+
+       switch (choix) {
+        case 1:  
+                   ////////////mini Meni de 1///////
+                  console.log("1. ajouter un candidat"); 
+                  console.log("2. ajouter plusieur candidat");     
+                  console.log("0. revnir au menu principale"); 
+                  choix1  = +read("enter votre choix :")
+
+                  switch (key) {
+                        case 1:
+                                
+                                break;
+                        case 2:
+                                
+                                break;
+                        case 0:
+                                
+                                break;        
+                  
+                        default:
+                          console.log("Choix n est pas valide");
+                                break;
+                  }
+
+        
+
+                break;
+       case 2:  
+                     
+                break;
+                case 3:  
+                     
+                break;
+                case 4:  
+                     
+                break;
+                case 5:  
+                     
+                break;
+                case 6:  
+                     
+                break;
+                case 7:
+                        case 8:  
+                     
+                break;  
+                     
+        default:
+                console.log("Choix n est pas valide");
+                break;
+       }
+      
+
+} while( choix !== 0 );
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+///////////////////////////Functions to be called/////////0
+
+function affichageCandidats() {
+         ////////////Menu ///////////
+        console.log("*********  elections ******");
         console.log("1. ajouter un condidat");
         console.log("2. afficer la list de candidats");
         console.log("3. voter ");
@@ -41,50 +112,5 @@ while (choix !== 8 ) {
         console.log("5. suprimer un candidat ");
         console.log("6. rechercher ");
         console.log("7. statistique ");
-        console.log("8. quitter");
-
-        
-        choix  = +read("enter votre choix :")
-
-
-    switch (choix) {
-    case 1:
-            /////////////////Menu de ajpouter un candidat//////
-           console.log("1. ajouter un condidat");
-           console.log("2. ajouter un plusieur candidat");
-           console.log("0. revenire au menu principale");
-        break;
-        case 2 :
-
-           console.log("test case 2");   
-        break;
-                case 3 :
-
-           console.log("test case 3");   
-        break;
-                case 4 :
-
-           console.log("test case 4");   
-        break;
-                case 5 :
-
-           console.log("test case 5");   
-        break;
-                case 6 :
-
-           console.log("test case 6");   
-        break;
-                case 7 :
-
-           console.log("test case 7");   
-        break;
-                case 8 :
-             
-           console.log("test case 8");   
-        break;
-          
-    default:
-        console.log("Choix invalide");
-        break;
-}
+        console.log("0. quitter");
 }
