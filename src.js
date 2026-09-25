12f6b90d15552442b8c1f864ae97529e16db4f0a
Asharfdev <87210166+Asharfdev@users.noreply.github.com>
2026-09-25 02:31:36 +0100

    Update src.js

--- src.js
+++ src.js
@@ -1,23 +1,90 @@
+const read = require("prompt-sync")();
+
+const candidats = [
+
+{
+cin : "AB1231",
+nom : "Boushaba",
+prenom : "Soufiane",
+partiPolitique : "Indépendant",
+age: 40,
+electeurs: []
+},
+{
+cin : "AB1232",
+nom : "Boushaba",
+prenom : "Soufiane",
+partiPolitique : "Indépendant",
+age: 40,
+electeurs: []
+},
+{
+cin : "AB1233",
+nom : "Boushaba",
+prenom : "Soufiane",
+partiPolitique : "Indépendant",
+age: 40,
+electeurs: []
+},
+];
 
 
+let choix = 0;
 
-////////////Menu principale///////////
-console.log("1. Ajouter un condidat");
-console.log("2. afficer las list de candidates");
-console.log("3. voter ");
-console.log("4. modifier les information ");
-console.log("5. suprimer un candidat ");
-console.log("6. rechercher ");
-console.log("7. statistique ");
+while (choix !== 8 ) {
+        ////////////Menu principale///////////
+        console.log("*********  e1lections ******");
+        console.log("1. ajouter un condidat");
+        console.log("2. afficer la list de candidats");
+        console.log("3. voter ");
+        console.log("4. modifier les information ");
+        console.log("5. suprimer un candidat ");
+        console.log("6. rechercher ");
+        console.log("7. statistique ");
+        console.log("8. quitter");
 
-
-///////////////Menu apres ajpouter un candidat//////
-
-console.log("1.Ajouter Un Candidat");
-console.log("2.Ajouter plusier candidat");
-console.log("0. revenire au menu parincipale");
-
-
+        
+        choix  = +read("enter votre choix :")
 
 
+    switch (choix) {
+    case 1:
+            /////////////////Menu de ajpouter un candidat//////
+           console.log("1. ajouter un condidat");
+           console.log("2. ajouter un plusieur candidat");
+           console.log("0. revenire au menu principale");
+        break;
+        case 2 :
 
+           console.log("test case 2");   
+        break;
+                case 3 :
+
+           console.log("test case 3");   
+        break;
+                case 4 :
+
+           console.log("test case 4");   
+        break;
+                case 5 :
+
+           console.log("test case 5");   
+        break;
+                case 6 :
+
+           console.log("test case 6");   
+        break;
+                case 7 :
+
+           console.log("test case 7");   
+        break;
+                case 8 :
+             
+           console.log("test case 8");   
+        break;
+          
+    default:
+        console.log("Choix invalide");
+        break;
+}
+}
